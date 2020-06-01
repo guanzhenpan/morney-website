efa3e9e938dd5d7cb2ce2bbc3e3041e4421c153f
panguanzhen <panguanzhen@qq.com>
2020-06-01 12:57:03 +0800

    deploy

---
+++ precache-manifest.8d7ffd0b0e63d0d3e579d6799ebe84f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40ea2a73fdb597bc9df",
+    "url": "/morney-website/css/app.6165a128.css"
+  },
+  {
+    "revision": "f96664410ac356e6bc522649543c7479",
+    "url": "/morney-website/images/qrcode.png"
+  },
+  {
+    "revision": "5de283163fd7e17f4fb7a88fc3168321",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "c40ea2a73fdb597bc9df",
+    "url": "/morney-website/js/app.65c9e803.js"
+  },
+  {
+    "revision": "ada8e3aae03252016a37",
+    "url": "/morney-website/js/chunk-vendors.1a43a34c.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
